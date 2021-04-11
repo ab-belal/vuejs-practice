d9411ac3692a29654a02ca2fe07615ae075186bb
AB Belal <belal@coderex.co>
2021-04-11 21:52:55 +0600

    list rendaring

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,21 @@
 var app = new Vue({
     el: '#vue-root',
     data: {
-        name: 'Jhon doe'
+        name: 'Jhon doe',
+        link1: {
+            text:"Facebook", title: "Go Facebook", url: "facebook.com"
+        },
+        link2: {
+            text:"Twitter", title: "Go Twitter", url: "twitter.com"
+        },
+        skills: [
+            {language: 'html', item: 2},
+            {language: 'css', item: 3},
+            {language: 'php', item: 4},
+            {language: 'jQuery', item: 5},
+            {language: 'vue.js', item: 6},
+            {language: 'react.js', item: 7},
+        ],
     },
 
     mounted: function() {
