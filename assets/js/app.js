00daaeb61b3b5f5bf61a8aa55f5f4e455204caaf
AB Belal <belal@coderex.co>
2021-04-14 21:11:37 +0600

    method completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#vue-root',
     data: {
+        newSkill: "",
         name: 'Jhon doe',
         link1: {
             text:"Facebook", title: "Go Facebook", url: "facebook.com"
@@ -8,14 +9,22 @@ var app = new Vue({
         link2: {
             text:"Twitter", title: "Go Twitter", url: "twitter.com"
         },
-        skills: [
-            {language: 'html', item: 2},
-            {language: 'css', item: 3},
-            {language: 'php', item: 4},
-            {language: 'jQuery', item: 5},
-            {language: 'vue.js', item: 6},
-            {language: 'react.js', item: 7},
-        ],
+        // skills: [
+        //     {language: 'html', item: 2},
+        //     {language: 'css', item: 3},
+        //     {language: 'php', item: 4},
+        //     {language: 'jQuery', item: 5},
+        //     {language: 'vue.js', item: 6},
+        //     {language: 'react.js', item: 7},
+        // ],
+        skills: ['html', 'css', 'css', 'php', 'jQuery', 'vue.js',]
+    },
+
+    methods: {
+        addNewSkill: function(){
+            this.skills.push(this.newSkill);
+            this.newSkill = '';
+        }
     },
 
     mounted: function() {
